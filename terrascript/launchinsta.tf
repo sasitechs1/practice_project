b671b0f927ccb5024a598e90c8fbad1fa813938e
Sasi0328 <112743050+Sasi0328@users.noreply.github.com>
2022-11-09 14:25:08 +0530

    Update launchinsta.tf

--- terrascript/launchinsta.tf
+++ terrascript/launchinsta.tf
@@ -1,13 +1,14 @@
 provider "aws"{
     region = "ap-south-1"
  }
+ 
  resource "aws_instance" "AWSEC2INSTANCE" {
     count = "1"
     ami = "ami-05c8ca4485f8b138a"
     instance_type= "t2.micro"
-    security_groups= [" launch-wizard-15"]
+    security_groups= ["launch-wizard-15"]
     key_name= "vijay"
     tags = {
     Name= "tomcat server" 
-   }    
+}    
 }
